d268b62ad856c3416af88fabfcc3e42a1a9f98bc
RoobScoobs <iruben229@gmail.com>
2016-07-12 10:12:01 -0400

    Initial set up w/ Sign Up
    Utilizing bootstrap to create sign up modal

---
+++ assets/scripts/homepage-events.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const homePageHandlers = () => {
+  $(".modal-fullscreen").on('show.bs.modal', function () {
+  setTimeout( function() {
+    $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+  }, 0);
+});
+$(".modal-fullscreen").on('hidden.bs.modal', function () {
+  $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+});
+
+};
+
+module.exports = {
+  homePageHandlers
+};
