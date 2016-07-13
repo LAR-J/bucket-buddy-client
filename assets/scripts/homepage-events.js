e6c3fdfaa94b7fe21b1b8df50dc0ca68c642306f
RoobScoobs <iruben229@gmail.com>
2016-07-13 08:26:23 -0400

    Add Sign-in feature to home page
    Create two separate modals (now we have sign-up and sign-in) and ajax
    calls work with existing forms

--- assets/scripts/homepage-events.js
+++ assets/scripts/homepage-events.js
@@ -1,12 +1,21 @@
 'use strict';
 
 const homePageHandlers = () => {
-  $(".modal-fullscreen").on('show.bs.modal', function () {
+  $(".modal-fullscreen.sign-up").on('show.bs.modal', function () {
   setTimeout( function() {
     $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
   }, 0);
 });
-$(".modal-fullscreen").on('hidden.bs.modal', function () {
+$(".modal-fullscreen.sign-up").on('hidden.bs.modal', function () {
+  $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+});
+
+$(".modal-fullscreen.sign-in").on('show.bs.modal', function () {
+setTimeout( function() {
+  $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+}, 0);
+});
+$(".modal-fullscreen.sign-in").on('hidden.bs.modal', function () {
   $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
 });
 
