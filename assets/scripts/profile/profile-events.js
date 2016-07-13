32b9365dc2926d2fd15933d123f1651d6c2f88bb
andreasantarl <andrea.santarl@gmail.com>
2016-07-13 16:07:43 -0400

    Promise-ifies update and get profile ajax req
    Turns ajax requests for update and get profile into promises
    
    Still need to work on promise-ifying create and destroy
    
    HTML is has correct names reflecting schema data fields
    HTML includes button to delete profile
    
    GET profile does not display in the UI, but the request works

--- assets/scripts/profile/profile-events.js
+++ assets/scripts/profile/profile-events.js
@@ -9,77 +9,101 @@ const ui = require('./profile-ui');
 const onCreateProfile = (event) => {
   event.preventDefault();
   let data = getFormFields(event.target);
-  console.log(data);
+//  console.log(data);
   api.createProfile(data)
   .done(ui.createProfileSuccess)
   .fail(ui.createProfileFailure);
 };
 
-const onUpdateProfile = (event) => {
-  event.preventDefault();
-  let data = getFormFields(event.target);
-  console.log(data);
-  api.updateProfile(data)
-  .done(ui.updateProfileSuccess)
-  .fail(ui.updateProfileFailure);
-};
-
-// const onSignOut = (event) => {
-//   event.preventDefault();
-//   api.signOut()
-//   .done(ui.signOutSuccess)
-//   .fail(ui.failure);
-// };
-
 // const onUpdateProfile = (event) => {
 //   event.preventDefault();
-//   // let data = getFormFields(event.target);
-//   new Promise = (resolve, reject) => {
 //   let data = getFormFields(event.target);
-//     api.updateProfile(data, (error, response) => {
-//       if (error) {
-//         reject(error);
-//       } else {
-//         resolve(response);
-//       }
-//     })
-//     .then((response) => {
-//       ui.updateProfileSuccess;
-//     })
-//     .then((response) => {
-//       api.showProfile;
-//     })
-//     .catch
-//   }
-//
-//
-//   }
-//
+//   console.log(data);
+//   api.updateProfile(data)
 //   .done(ui.updateProfileSuccess)
 //   .fail(ui.updateProfileFailure);
 // };
 
-
-// const onSignIn = (event) => {
+// const onShowProfile = (event) => {
 //   event.preventDefault();
-//   let data = getFormFields(event.target);
-//   api.signIn(data)
-//   .done(ui.signInSuccess)
-//   .fail(ui.failure);
+//   api.showProfile()
+//   .done(ui.showProfileSuccess)
+//   .fail(ui.showProfileFailure);
 // };
 //
-// const onSignOut = (event) => {
-//   event.preventDefault();
-//   api.signOut()
-//   .done(ui.signOutSuccess)
-//   .fail(ui.failure);
-// };
+const onDeleteProfile = (event) => {
+  event.preventDefault();
+  // let buttonId = $(event.target).attr('data-id');
+  api.deleteProfile()
+  .done(ui.deleteProfileSuccess)
+  .fail(ui.deleteProfileFailure);
+};
+
+//BEGINNING OF PROMISES
+const onShowProfile = () => {
+  // let data = getFormFields(event.target);
+  return api.showProfile()
+    .then(ui.showProfileSuccess)
+    .catch(error => console.error(error))
+};
+
+const onUpdateProfile = (event) => {
+  event.preventDefault();
+  let data = getFormFields(event.target);
+  api.updateProfile(data)
+  .then(onShowProfile)
+  .catch(error => console.error(error))
+};
+//
 //
 
+//   //   .then((response) => {
+//   //     ui.updateProfileSuccess;
+//   //   })
+//   //   .then((response) => {
+//   //     api.showProfile;
+//   //   })
+//   //   .catch
+//   // }
+//   // }
+// //
+// //   .done(ui.updateProfileSuccess)
+// //   .fail(ui.updateProfileFailure);
+// // };
+//
+//
+// // const onSignIn = (event) => {
+// //   event.preventDefault();
+// //   let data = getFormFields(event.target);
+// //   api.signIn(data)
+// //   .done(ui.signInSuccess)
+// //   .fail(ui.failure);
+// // };
+// //
+// // const onSignOut = (event) => {
+// //   event.preventDefault();
+// //   api.signOut()
+// //   .done(ui.signOutSuccess)
+// //   .fail(ui.failure);
+// // };
+// //
+//
 
 const addProfileHandlers = () => {
   $('#create-profile').on('submit', onCreateProfile);
+  // $('#update-profile').on('submit', function (event){
+  //      event.preventDefault();
+  //      let data = getFormFields(event.target);
+  //     onUpdateProfile(data)
+    //  .then(ui.updateProfileSuccess)
+      // .then(() => {
+      //   console.log("hello");
+      //   return onShowProfile();
+      // })
+      // .catch(console.error)
+//  });
   $('#update-profile').on('submit', onUpdateProfile);
+  $('#delete-profile').on('click', onDeleteProfile);
   // $('#sign-out').on('submit', onSignOut);
   // $('#change-password').on('submit', onChangePassword);
 };
