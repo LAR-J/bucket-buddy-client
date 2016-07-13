9495f6a87ed5b099c6b798e9082832620b9de8b8
RoobScoobs <iruben229@gmail.com>
2016-07-13 18:54:58 -0400

    Update profile tab in navigation
    Add create form attached in modals

--- assets/scripts/user-events.js
+++ assets/scripts/user-events.js
@@ -10,6 +10,15 @@ const userEventHandlers = () => {
     $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
     $('.change-password').find("input[type=password]").val('');
   });
+  $(".create-profile").on('show.bs.modal', function () {
+  setTimeout( function() {
+    $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+  }, 0);
+  });
+  $(".create-profile").on('hidden.bs.modal', function () {
+    $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+    $('.create-profile').find("input", "textarea", "select").val('');
+  });
 };
 
 module.exports = {
