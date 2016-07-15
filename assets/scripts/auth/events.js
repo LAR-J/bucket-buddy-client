a195258ea0d7c3e7ebb10c72d9e0ea7f75893097
andreasantarl <andrea.santarl@gmail.com>
2016-07-14 20:53:17 -0400

    Creates error handling for change password
    Change password displays error
    Does not allow people to set password to empty

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -36,9 +36,13 @@ const onSignOut = (event) => {
 const onChangePassword = (event) => {
   event.preventDefault();
   let data = getFormFields(event.target);
-  api.changePassword(data)
-  .done(ui.success)
-  .fail(ui.failure);
+  if(data.passwords.new) {
+    api.changePassword(data)
+    .done(ui.changePasswordSuccess)
+    .fail(ui.changPasswordFailure);
+  } else {
+    $("#change-password-message").html("Please enter a password.");
+  }
 };
 
 
